ebfbb09ca4bc99c1cae33f58af71fd17a19dd24b
Henk Joubert <jouberthenk@gmail.com>
2014-11-13 16:23:41 +0200

    Fix whitesapce

--- setup.sh
+++ setup.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 realpath() {
-	[[ $1 = /* ]] && echo $1 || echo "$PWD/${1#./}"
+    [[ $1 = /* ]] && echo $1 || echo "$PWD/${1#./}"
 }
 
 SCRIPT_PATH=`realpath $0`
 DOTFILES=`dirname $SCRIPT_PATH`
 
 if [ ! -d ~/.vim ]; then
-	mkdir ~/.vim
+    mkdir ~/.vim
 fi
 
 if [ ! -d ~/.vim/backup ]; then
-	mkdir ~/.vim/backup
+    mkdir ~/.vim/backup
 fi
 
 if [ ! -e ~/.vim/autoload/pathogen.vim ]; then
@@ -21,18 +21,18 @@ if [ ! -e ~/.vim/autoload/pathogen.vim ]; then
 fi
 
 if [ ! -e ~/.vimrc ]; then
-	ln -s "$DOTFILES/vimrc" ~/.vimrc
-	echo ".vimrc installed successfully"
+    ln -s "$DOTFILES/vimrc" ~/.vimrc
+    echo ".vimrc installed successfully"
 fi
 
 if [ ! -e ~/.zshrc ]; then
-	ln -s "$DOTFILES/zshrc" ~/.zshrc
-	echo ".zshrc installed successfully"
+    ln -s "$DOTFILES/zshrc" ~/.zshrc
+    echo ".zshrc installed successfully"
 fi
 
 if [ ! -e ~/.gitconfig ]; then
-	ln -s "$DOTFILES/gitconfig" ~/.gitconfig
-	echo ".gitconfig installed successfully"
+    ln -s "$DOTFILES/gitconfig" ~/.gitconfig
+    echo ".gitconfig installed successfully"
 fi
 
 
