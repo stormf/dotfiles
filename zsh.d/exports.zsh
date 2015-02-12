fdfd03bb26ced3c6fbda648c33ff181187dc4948
Storm <storm@meep.pink>
2015-02-12 21:20:08 +0200

    Clean up some stuff

--- zsh.d/exports.zsh
+++ zsh.d/exports.zsh
@@ -3,3 +3,4 @@ export EDITOR=vim
 export JAVA_HOME=$(/usr/libexec/java_home -v 1.8)
 export PATH=~/bin:/usr/local/bin:/usr/local/sbin:$PATH
 export PIP_DOWNLOAD_CACHE=~/.cache/pip/
+export PERSONAL_GIT=~/personal/git/
