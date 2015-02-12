61d30c7b9db6587b308854f489eaa2ca1ddd4950
Henk Joubert <henk@yola.com>
2015-02-12 21:47:59 +0200

    Only export JAVA_HOME on osx

--- zsh.d/exports.zsh
+++ zsh.d/exports.zsh
@@ -1,6 +1,9 @@
 #exports
 export EDITOR=vim
-export JAVA_HOME=$(/usr/libexec/java_home -v 1.8)
 export PATH=~/bin:/usr/local/bin:/usr/local/sbin:$PATH
 export PIP_DOWNLOAD_CACHE=~/.cache/pip/
 export PERSONAL_GIT=~/personal/git/
+
+if [ $OSX -eq 1 ]; then
+    export JAVA_HOME=$(/usr/libexec/java_home -v 1.8)
+fi
