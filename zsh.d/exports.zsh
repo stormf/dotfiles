7b185e94034068b12ef707539c851f8b8b52ac79
Henk Joubert <henk@yola.com>
2015-02-18 10:00:55 +0200

    Add locale exports

--- zsh.d/exports.zsh
+++ zsh.d/exports.zsh
@@ -6,4 +6,8 @@ export PERSONAL_GIT=~/personal/git/
 
 if [[ $OSX -eq 1 ]]; then
     export JAVA_HOME=$(/usr/libexec/java_home -v 1.8)
+
+    # locale
+    export LC_ALL=en_US.UTF-8
+    export LANG=en_US.UTF-8
 fi
