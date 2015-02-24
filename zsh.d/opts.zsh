e6ac88ca3cbeaa8be1b3946028148fab32b88278
Storm <storm@meep.pink>
2015-02-24 12:24:16 +0200

    add reportime

--- zsh.d/opts.zsh
+++ zsh.d/opts.zsh
@@ -15,6 +15,9 @@ setopt sharehistory # imports new commands and appends typed commands to history
 setopt hist_expire_dups_first # when trimming history, lose oldest duplicates first
 setopt hist_find_no_dups # When searching history don't display results already cycled through twice
 setopt hist_verify # don't execute, just expand history
+setopt hist_save_no_dups # don't show dups even if not contiguous
+
+REPORTTIME=2 # report time if execution exceeds amount of seconds
 
 # Completion
 setopt completealiases
