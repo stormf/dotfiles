e8531ffb43a1e8e29f9a226de98e9c25ceba0bf5
Henk Joubert <henk@yola.com>
2015-03-16 11:58:21 +0200

    Add git-clean function
    to delete branches already merged into master

--- zsh.d/functions.zsh
+++ zsh.d/functions.zsh
@@ -4,3 +4,14 @@ function fuck() {
     echo ; echo " (╯°□°）╯︵$(echo "$2"|toilet -f term -F rotate)"; echo
   fi
 }
+
+parse_git_branch () {
+        git branch 2> /dev/null | grep "*" | sed -e 's/* \(.*\)/ (\1)/g'
+}
+
+function git-clean() {
+    CURRENT_BRANCH=`parse_git_branch`
+    git checkout master
+    git branch --merged master | grep -v "\* master" | xargs -n 1 git branch -d
+    git checkout $CURRENT_BRANCH
+}
--- zsh.d/prompt.zsh
+++ zsh.d/prompt.zsh
@@ -3,11 +3,6 @@ autoload -U promptinit
 promptinit
 autoload -U colors && colors
 
-# Stuff for git
-parse_git_branch () {
-        git branch 2> /dev/null | grep "*" | sed -e 's/* \(.*\)/ (\1)/g'
-}
-
 parse_ve () {
     #handle_virtualenv
     PARENT=`dirname $VIRTUAL_ENV` 2> /dev/null
