cf38b6943094303eaf812ce5932e65c9401e4377
Storm <storm@meep.pink>
2015-03-18 11:01:10 +0200

    yola_clone

--- zsh.d/yola.zsh
+++ zsh.d/yola.zsh
@@ -24,5 +24,29 @@ yo() {
     cd $YOLA_GIT/$1
 }
 
+# git config --global url.http://git.ct.yola.net/git/.insteadOf yolact:
+# git config --global url.git@github.com:.insteadOf gh:
+function yola_clone() {(
+    set -ex
+    local app=$1
+    host git.ct.yola.net &> /dev/null
+    local ct=$?
+    if [ $ct -eq 0 ]; then
+        git clone "yolact:yola/$app.git"
+    else
+        git clone "gh:yola/$app.git"
+    fi
+    cd "$app"
+    git config user.email henk@yola.com
+    if [ $ct -eq 0 ]; then
+        git remote rename origin ctmirror
+        git remote add origin "gh:yola/$app.git"
+    else
+        git remote add ctmirror "yolact:yola/$app.git"
+    fi
+    git fetch origin
+    git branch --set-upstream-to origin/master master
+)}
+
 alias odoor="curl -d \"Outer=open-door\" ${YOLA_DOOR_URL}"
 alias idoor="curl -d \"Inner=open-door\" ${YOLA_DOOR_URL}"
