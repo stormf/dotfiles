887eb71911c045a1074245af49c9afb4fe729940
Storm <storm@meep.pink>
2015-04-08 17:02:37 +0200

    Add git diff-highlight to path

--- zsh.d/exports.zsh
+++ zsh.d/exports.zsh
@@ -1,8 +1,10 @@
 #exports
 export EDITOR=vim
-export PATH=~/bin:/usr/local/bin:/usr/local/sbin:/usr/local/share/git-core/contrib/diff-highlight:$PATH
+export LOCAL_BIN_PATH=~/bin:/usr/local/bin:/usr/local/sbin:/usr/local/share/git-core/contrib/diff-highlight
+export PATH=$LOCAL_BIN_PATH:$PATH
 export PIP_DOWNLOAD_CACHE=~/.cache/pip/
 export PERSONAL_GIT=~/personal/git/
+export DOTFILES=~/personal/git/dotfiles
 
 if [[ $OSX -eq 1 ]]; then
     export JAVA_HOME=$(/usr/libexec/java_home -v 1.8)
