8acbcbcc453995831f86a905578385dbb218b9b9
Storm Joubert <storm@yola.com>
2017-01-30 17:48:12 +0200

    Add alias to view ssl certs

--- zsh.d/functions.zsh
+++ zsh.d/functions.zsh
@@ -40,3 +40,7 @@ function ne() {
     shift
     "$(npm bin)/$cmd" "$@"
 }
+
+function view_ssl_cert() {
+    echo | openssl s_client -showcerts -servername "$1" -connect "$1":443 2>/dev/null | openssl x509 -inform pem -noout -text
+}
