bb0a77bcf8b2f8835a186f61005184fa9d18dfe8
Storm Joubert <storm@yola.com>
2017-04-19 11:17:02 +0200

    Add tiny-care-terminal settings
    also gpg 2.1 isn't in the usual /usr/local/bin for some reason :/

--- zsh.d/exports.zsh
+++ zsh.d/exports.zsh
@@ -5,6 +5,7 @@ export GPG_TTY
 
 export EDITOR=vim
 export LOCAL_BIN_PATH=~/bin:/usr/local/bin:/usr/local/sbin:/usr/local/share/git-core/contrib/diff-highlight
+export LOCAL_BIN_PATH="/usr/local/opt/gnupg@2.1/bin:$LOCAL_BIN_PATH"
 export PATH=$LOCAL_BIN_PATH:$PATH
 export PERSONAL_GIT=~/personal
 export DOTFILES=~/personal/dotfiles
@@ -21,3 +22,17 @@ if [[ $OSX -eq 1 ]]; then
 	export CFLAGS="-I/usr/local/opt/openssl/include"
 	export SWIG_FEATURES="-cpperraswarn -includeall -I/usr/local/opt/openssl/include"
 fi
+
+# tiny-care-terminal settings
+# List of accounts to read the last tweet from, comma separated
+# The first in the list is read by the party parrot.
+export TTC_BOTS='tinycarebot,selfcare_bot,magicrealismbot'
+
+# List of folders to look into for `git` commits, comma separated.
+export TTC_REPOS='~/work/git'
+
+export TTC_WEATHER='Cape Town'
+export TTC_CELSIUS=true
+export TTC_APIKEYS=true
+# Refresh the dashboard every 20 minutes.
+export TTC_UPDATE_INTERVAL=20
