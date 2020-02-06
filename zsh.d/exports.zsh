13d31a191ba65c80c1c291116cb483fa3ea525d6
Storm Joubert <storm.joubert@takealot.com>
2020-02-06 08:49:37 +0200

    source gcloud completions

--- zsh.d/exports.zsh
+++ zsh.d/exports.zsh
@@ -7,6 +7,9 @@ export EDITOR=vim
 export LOCAL_BIN_PATH=~/bin:/usr/local/bin:/usr/local/sbin:/usr/local/share/git-core/contrib/diff-highlight
 export LOCAL_BIN_PATH="/usr/local/opt/curl/bin:$LOCAL_BIN_PATH"
 export PATH=$LOCAL_BIN_PATH:$PATH
+
+source ~/google-cloud-sdk/path.zsh.inc
+
 export PERSONAL_GIT=~/personal
 export DOTFILES=~/personal/dotfiles
 
