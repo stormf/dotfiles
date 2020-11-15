5fded506e3675529586607f862e276ddba6776a2
Storm Joubert <storm@scrypt.local>
2020-11-15 12:32:41 +0200

    updoots
    readme file needed a bit more info on doing the intial setup stuff like
    setting up envchain. setup script already updates the submodules anyway.
    include the homebrew setup command
    
    only source google sdk stuff if present
    
    zsh sharehistory is supposed to be exclusive with the other history
    options

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
 # My dotfile repo #
 
-Clone with submodules for `./setup` to work correctly
-`git clone --recurse-submodules https://github.com/stormf/dotfiles.git`
+```
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+```
 
+Generate a github access token at https://github.com/settings/tokens
+
+```
+envchain --set homebrew HOMEBREW_GITHUB_API_TOKEN
+homebrew.HOMEBREW_GITHUB_API_TOKEN: < your token goes here >
+```
+
+iTerm2 fairyfloss theme from: https://github.com/aquartier/fairyfloss
--- zsh.d/opts.zsh
+++ zsh.d/opts.zsh
@@ -7,10 +7,10 @@ setopt noclobber
 
 # History
 HISTFILE=~/.histfile
-HISTSIZE=5000
-SAVEHIST=5000
-setopt appendhistory # Allow multiple terminal sessions to all append to one zsh command history
-setopt inc_append_history # Add comamnds as they are typed, don't wait until shell exit
+HISTSIZE=50000
+SAVEHIST=50000
+#setopt appendhistory # Allow multiple terminal sessions to all append to one zsh command history
+#setopt inc_append_history # Add comamnds as they are typed, don't wait until shell exit
 setopt sharehistory # imports new commands and appends typed commands to history
 setopt hist_expire_dups_first # when trimming history, lose oldest duplicates first
 setopt hist_find_no_dups # When searching history don't display results already cycled through twice
